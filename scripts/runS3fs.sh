77140d66a7b7112d8983d460eca11a52cabbc38d
Bertrand Roussel <broussel@sierrawireless.com>
2015-12-29 16:02:27 +0100

    Options MOUNTDIR & MOUNTOPTS

--- scripts/runS3fs.sh
+++ scripts/runS3fs.sh
@@ -1,1 +1,14 @@
-mkdir -p /home/shared/s3 && mkdir -p /tmp && s3fs $BUCKETNAME /home/shared/s3 -o use_cache=/tmp -o allow_other -o umask=0002 -o use_rrs -f
+#!/bin/bash -e
+
+MOUNTDIR=${MOUNTDIR:-/home/shared/s3}
+
+mkdir -p $MOUNTDIR
+mkdir -p /tmp
+s3fs $BUCKETNAME $MOUNTDIR \
+    -o use_cache=/tmp \
+    -o allow_other \
+    -o umask=0002 \
+    -o use_rrs \
+    -f \
+    $MOUNTOPTS
+
